1f170173d3f39a2f1fbe34d6f9be2dff65ac89b8
KeitoKunishima <96482263+KeitoKunishima@users.noreply.github.com>
2022-01-26 00:49:08 +0900

    Update .myled.ko.cmd

--- .myled.ko.cmd
+++ .myled.ko.cmd
@@ -1,1 +1,5 @@
+
+/*  COPYING   GPL v3.0
+    Copyright © 2021 RyuichiUeda, KeitoKunishima All rights reserved.  */
+
 cmd_/home/ubuntu/myled/myled.ko := ld -r  -EL  -maarch64elf  --build-id  -T ./scripts/module-common.lds -T ./arch/arm64/kernel/module.lds -o /home/ubuntu/myled/myled.ko /home/ubuntu/myled/myled.o /home/ubuntu/myled/myled.mod.o;  true
