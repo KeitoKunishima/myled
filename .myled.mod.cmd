c3f6189943b099d07517db9afe87e74c0d758cc6
KeitoKunishima <96482263+KeitoKunishima@users.noreply.github.com>
2022-01-27 22:32:54 +0900

    Update .myled.mod.cmd

--- .myled.mod.cmd
+++ .myled.mod.cmd
@@ -1,1 +1,6 @@
+
+/*  COPYING   GPL v3.0
+    Copyright © 2021 RyuichiUeda, KeitoKunishima   All rights reserved.  */
+
+
 cmd_/home/ubuntu/myled/myled.mod := { echo  /home/ubuntu/myled/myled.o;  echo; } > /home/ubuntu/myled/myled.mod
