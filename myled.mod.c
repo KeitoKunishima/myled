73c80833873ee84e0eb188fe35d8fcd5056d35ac
KeitoKunishima <96482263+KeitoKunishima@users.noreply.github.com>
2022-01-27 22:37:22 +0900

    Update myled.mod.c

--- myled.mod.c
+++ myled.mod.c
@@ -1,4 +1,8 @@
 
+/*  COPYING   GPL v3.0
+    Copyright © 2021 RyuichiUeda, KeitoKunishima   All rights reserved.  */
+
+
 #include <linux/build-salt.h>
 #include <linux/module.h>
 #include <linux/vermagic.h>
